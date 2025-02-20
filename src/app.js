f9d65aa6893941c5ca924d2d83bc6741f55063fd
DocGus <150487562+DocGus@users.noreply.github.com>
2025-02-20 04:20:39 +0000

    cambios nataly

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+  let action = ['se comió', 'destruyo', 'aplastó', 'rompió'];
+  let what = ['mi tarea', 'mi teléfono', 'el auto'];
+  let when = ['antes de la clase', 'mientras dormía', 'cuando hacía ejercicio', 'durante el almuerzo', 'mientras rezaba'];
+
+  let excuse = `${who[Math.floor(Math.random() * who.length)]}
+                             ${action[Math.floor(Math.random() * action.length)]}
+                             ${what[Math.floor(Math.random() * what.length)]}
+                             ${when[Math.floor(Math.random() * when.length)]}.`;
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
